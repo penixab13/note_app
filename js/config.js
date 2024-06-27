5ec0fe7e396a9dda1abbfbbc761b69a4c11628c3
penixab13 <115022449+penixab13@users.noreply.github.com>
2024-06-27 13:56:59 +0100

    update config

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_Vi6V6BGu0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7pbchm4mrcvor886bjtstq06eh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
